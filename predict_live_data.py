0fdf4165813904f5763a43959b11fcb0f5a96d9a
RinkuAkash <akashrinku2@outlook.com>
2020-05-20 11:12:27 +0530

    Refactored. add comments and removed main function

--- predict_live_data.py
+++ predict_live_data.py
@@ -1,21 +1,28 @@
+# importing required libraries
 import json
 import time
 import pandas as pd
 
 from datetime import datetime
-from flask import Flask, Response, render_template, make_response
+from flask import Flask, render_template, make_response
 
 from pyspark import SparkContext
 from pyspark.sql import SQLContext
 from kafka import KafkaConsumer
 from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.regression import LinearRegressionModel
+from pyspark.sql.types import DoubleType
 
-
+# creating spark context
 sc = SparkContext(appName="StockPricePrediction")
-sqlCtx = SQLContext(sc)
+sqlContext = SQLContext(sc)
+
+# vector Assembler is to transform values to fit in model
 vectorAssembler = VectorAssembler(inputCols=['open', 'high', 'low'], outputCol='features')
-model = LinearRegressionModel.load('model')
+model = LinearRegressionModel.load('model') # loading machine learning model
+
+
+# function to get predicted stock price from kafka
 def predict_price(vectorAssembler, model):
     consumer = KafkaConsumer('stockprices'
                              , bootstrap_servers=['localhost:9092']
@@ -23,41 +30,55 @@ def predict_price(vectorAssembler, model):
                              , consumer_timeout_ms=3000
                             )
     for message in consumer:
+
         record = json.loads(message.value)
-        series = pd.Series(record, dtype='float')
+
+        series = pd.Series(record, dtype=float) # message.value contains key value pair that support to convert into pandas series
         dataframe = series.to_frame()
         dataframe = dataframe.T
-        dataframe.columns = ['open', 'high', 'low', 'close', 'volumn']
-        sdf = sqlCtx.createDataFrame(dataframe)
-        vectored_dataframe = vectorAssembler.transform(sdf)
+        dataframe.columns = ['open', 'high', 'low', 'close', 'volumn'] # modifying columns names for easy access
+
+        spark_df = sqlContext.createDataFrame(dataframe) # creating spark dataframe
+
+        vectored_dataframe = vectorAssembler.transform(spark_df)
         vectored_dataframe = vectored_dataframe.select(['features', 'close'])
-        predictions = model.transform(vectored_dataframe)
-        predict_value = predictions.select('prediction').collect()[0].__getitem__("prediction")
-        close_value = predictions.select('close').collect()[0].__getitem__('close')
-        return round(predict_value, 2), close_value
-    return 0,0
+
+        predictions = model.transform(vectored_dataframe) # predictions on real time data
+
+        predict_value = predictions.select('prediction').collect()[0].__getitem__("prediction") # predicted value from dataframe
+        close_value = predictions.select('close').collect()[0].__getitem__('close') # close value from dataframe
+        date_time = message.key.decode('utf-8') # decoding datetime from key in message
+
+        return round(predict_value, 2), round(close_value, 2), date_time
+
+# To avoid errors after emptying message in kafka consumer server following values are returned
+    predict_value = 0
+    close_value = 0
+    date_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return predict_value, close_value, date_time
 
 
+# creating flask application
 app = Flask(__name__)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
 
+# url data to get data on index page
 @app.route('/data', methods=['GET', 'POST'])
 def data():
-    stock_price, close_price = predict_price(vectorAssembler, model)
+    stock_price, close_price, date_time = predict_price(vectorAssembler, model)
 
-    data = [time.time() * 1000, stock_price, close_price]
+    date_time = int(datetime.strptime(date_time, '%Y-%m-%d %H:%M:%S').strftime('%s')) * 1000
+
+    data = [date_time, stock_price, close_price]
 
     response = make_response(json.dumps(data))
 
     response.content_type = 'application/json'
 
-    time.sleep(5)
+    time.sleep(2)
     return response
-
-if __name__=='__main__':
-    app.run(host='127.0.0.1', port="5002", debug=True, threaded=True)
-
