854881385e083ee952e84c491c9416a3c876c62a
RinkuAkash <akashrinku2@outlook.com>
2020-05-20 11:08:26 +0530

    Refactored. added comments

--- live_data.py
+++ live_data.py
@@ -1,16 +1,16 @@
-from alpha_vantage.timeseries import TimeSeries
-from kafka import KafkaProducer
+from alpha_vantage.timeseries import TimeSeries # alpha vantage api to get realtime stock data
+from kafka import KafkaProducer # kafka producer to send data to kafka server
 import json
 import time
 
 def get_data():
     with open("alpha_vantage_key") as file:
         lines = file.readlines()
-    time_series = TimeSeries(key=lines[0])
-    data, meta_data = time_series.get_intraday(symbol='GOOG', interval='1min')
+    time_series = TimeSeries(key=lines[0]) # assigning API key to TimeSeries
+    data, meta_data = time_series.get_intraday(symbol='GOOG', interval='1min', outputsize='full') # Getting Google data
     return data
 
-def publish_message(producer_instance, topic_name, key, value):
+def publish_message(producer_instance, topic_name, key, value): # function to send data to kafka Server
     try:
         key_bytes = bytes(key, encoding='utf-8')
         producer_instance.send(topic_name, key=key_bytes, value=value)
@@ -20,7 +20,7 @@ def publish_message(producer_instance, topic_name, key, value):
         print("Exception in publishing message")
         print(str(ex))
 
-def connect_kafka_producer():
+def connect_kafka_producer(): # Function to connect and configure kafka producer 
     _producer = None
     try:
         _producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda value: json.dumps(value).encode('utf-8'))
@@ -31,10 +31,9 @@ def connect_kafka_producer():
         return _producer
 
 if __name__=="__main__":
-    while True:
-        data = get_data()
-        if len(data) > 0:
-            kafka_producer = connect_kafka_producer()
-            for key in data:
-                publish_message(kafka_producer, 'stockprices', key, data[key])
-                time.sleep(5)
+    data = get_data()
+    if len(data) > 0:
+        kafka_producer = connect_kafka_producer()
+        for key in sorted(data):
+            publish_message(kafka_producer, 'stockprices', key, data[key])
+            time.sleep(3)
